ab09507496137ef27aef94923f5066920aad2853
marcoamolinaro <molinaromarcoaurelio@gmail.com>
2023-05-02 16:30:48 -0300

    Add requestParam endpoint

--- spring-boot-demo/src/main/java/br/com/scm/springbootdemo/controller/HomeController.java
+++ spring-boot-demo/src/main/java/br/com/scm/springbootdemo/controller/HomeController.java
@@ -3,6 +3,7 @@ package br.com.scm.springbootdemo.controller;
 import br.com.scm.springbootdemo.model.User;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -24,5 +25,11 @@ public class HomeController {
     public String pathVariable(@PathVariable String id,
                                @PathVariable("id2") String name) {
         return "The path Variable is " + id + " " + name;
+    }
+
+    @GetMapping("/requestParam")
+    public String requestParams(@RequestParam String name,
+                                @RequestParam(value = "email", required = false, defaultValue = "") String emailId) {
+        return "Your name is " + name + " " + emailId);
     }
 }
