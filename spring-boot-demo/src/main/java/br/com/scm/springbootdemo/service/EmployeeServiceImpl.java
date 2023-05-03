de35e9bd91650c92e08ab8c42151a312f8c77248
marcoamolinaro <molinaromarcoaurelio@gmail.com>
2023-05-03 08:42:18 -0300

    Add get employee by id endpoint

--- spring-boot-demo/src/main/java/br/com/scm/springbootdemo/service/EmployeeServiceImpl.java
+++ spring-boot-demo/src/main/java/br/com/scm/springbootdemo/service/EmployeeServiceImpl.java
@@ -28,4 +28,13 @@ public class EmployeeServiceImpl implements EmployeeService {
     public List<Employee> getAllEmployees() {
         return employees;
     }
+
+    @Override
+    public Employee getEmployeeById(String id) {
+        return employees
+                .stream()
+                .filter(employee -> employee.getEmployeeId().equalsIgnoreCase(id))
+                .findFirst()
+                .get();
+    }
 }
