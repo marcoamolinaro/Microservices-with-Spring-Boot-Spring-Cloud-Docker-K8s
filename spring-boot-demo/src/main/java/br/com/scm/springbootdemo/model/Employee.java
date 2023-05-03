d57078882794e610852928a23dc2d7b8139e2962
marcoamolinaro <molinaromarcoaurelio@gmail.com>
2023-05-03 14:20:04 -0300

    Add Data Filtering

--- spring-boot-demo/src/main/java/br/com/scm/springbootdemo/model/Employee.java
+++ spring-boot-demo/src/main/java/br/com/scm/springbootdemo/model/Employee.java
@@ -1,11 +1,16 @@
 package br.com.scm.springbootdemo.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@JsonIgnoreProperties({"department"})
 public class Employee {
 
     private String employeeId;
     private String firstName;
     private String lastName;
     private String emailId;
+    //@JsonIgnore
     private String department;
 
     public Employee() {
