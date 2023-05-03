34ee0307b95bed558147c3cfa4d2055ac0c6ff84
marcoamolinaro <molinaromarcoaurelio@gmail.com>
2023-05-03 16:23:11 -0300

    Add Get employee methods with JPA

--- spring-boot-demo/src/main/java/br/com/scm/springbootdemo/service/EmployeeV2ServiceImpl.java
+++ spring-boot-demo/src/main/java/br/com/scm/springbootdemo/service/EmployeeV2ServiceImpl.java
@@ -9,6 +9,7 @@ import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
 @Service
 public class EmployeeV2ServiceImpl implements EmployeeService {
@@ -33,12 +34,23 @@ public class EmployeeV2ServiceImpl implements EmployeeService {
 
     @Override
     public List<Employee> getAllEmployees() {
-        return null;
+        List<EmployeeEntity> employeeEntityList = employeeRepository.findAll();
+        List<Employee> employees = employeeEntityList.stream()
+                .map(employeeEntity -> {
+                    Employee employee = new Employee();
+                    BeanUtils.copyProperties(employeeEntity, employee);
+                    return employee;
+                })
+                .collect(Collectors.toList());
+        return employees;
     }
 
     @Override
     public Employee getEmployeeById(String id) {
-        return null;
+        EmployeeEntity employeeEntity = employeeRepository.findById(id).get();
+        Employee employee = new Employee();
+        BeanUtils.copyProperties(employeeEntity, employee);
+        return employee;
     }
 
     @Override
