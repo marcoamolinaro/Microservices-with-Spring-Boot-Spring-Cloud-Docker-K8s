a827b6bc192c1c6776210a5368705cb4e6c04666
marcoamolinaro <molinaromarcoaurelio@gmail.com>
2023-05-03 18:05:40 -0300

    First commit productservice

---
+++ productService/src/test/java/br/com/scm/productservice/ProductServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.scm.productservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
