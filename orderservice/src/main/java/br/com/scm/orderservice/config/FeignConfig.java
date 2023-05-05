01255d6499bd10bf620ea439dc3c87cf8f0c9fcf
Marco A Molinaro <molinaromarcoaurelio@gmail.com>
2023-05-05 00:08:42 -0300

    Implementing ErrorDecoder to handle Exceptions

---
+++ orderservice/src/main/java/br/com/scm/orderservice/config/FeignConfig.java
@@ -0,0 +1,14 @@
+package br.com.scm.orderservice.config;
+
+import br.com.scm.orderservice.external.client.decoder.CustomErrorDecoder;
+import feign.codec.ErrorDecoder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class FeignConfig {
+    @Bean
+    ErrorDecoder errorDecoder() {
+        return new CustomErrorDecoder();
+    }
+}
