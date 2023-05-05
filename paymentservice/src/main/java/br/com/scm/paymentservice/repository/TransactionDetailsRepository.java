0db1521acc4b19d8a5893c189e8393ce82f253df
marcoamolinaro <molinaromarcoaurelio@gmail.com>
2023-05-05 15:59:30 -0300

    Create Payment Service

---
+++ paymentservice/src/main/java/br/com/scm/paymentservice/repository/TransactionDetailsRepository.java
@@ -0,0 +1,9 @@
+package br.com.scm.paymentservice.repository;
+
+import br.com.scm.paymentservice.entity.TransactionDetails;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TransactionDetailsRepository extends JpaRepository<TransactionDetails, Long> {
+}
---
+++ paymentservice/src/main/java/br/com/scm/paymentservice/service/PaymentService.java
@@ -0,0 +1,4 @@
+package br.com.scm.paymentservice.service;
+
+public interface PaymentService {
+}
