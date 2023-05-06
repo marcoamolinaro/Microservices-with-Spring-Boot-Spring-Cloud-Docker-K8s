c75a8332bd7594e35c2ff33094c04628ba0d73e2
Marco A Molinaro <molinaromarcoaurelio@gmail.com>
2023-05-06 19:37:12 -0300

    Add API Gateway service

---
+++ cloudgateway/src/test/java/br/com/scm/cloudgateway/CloudgatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.scm.cloudgateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudgatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
