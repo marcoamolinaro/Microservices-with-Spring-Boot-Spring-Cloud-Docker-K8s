d6183996963a8b5af20db6e4ef98ff3b111382e2
marcoamolinaro <molinaromarcoaurelio@gmail.com>
2023-05-08 10:26:30 -0300

    Implementing Circuit Breaker in API gateway

--- orderservice/src/main/java/br/com/scm/orderservice/external/client/PaymentService.java
+++ orderservice/src/main/java/br/com/scm/orderservice/external/client/PaymentService.java
@@ -1,13 +1,20 @@
 package br.com.scm.orderservice.external.client;
 
+import br.com.scm.orderservice.exception.CustomException;
 import br.com.scm.orderservice.external.request.PaymentRequest;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
+@CircuitBreaker(name="external", fallbackMethod = "fallback")
 @FeignClient(name = "PAYMENT-SERVICE/payments")
 public interface PaymentService {
     @PostMapping
     ResponseEntity<Long> doPayment(@RequestBody PaymentRequest paymentRequest);
+
+    default void fallback(Exception e) {
+        throw new CustomException("Payment Service is not available", "UNAVAILABLE", 500);
+    }
 }
--- orderservice/src/main/java/br/com/scm/orderservice/external/client/ProductService.java
+++ orderservice/src/main/java/br/com/scm/orderservice/external/client/ProductService.java
@@ -1,13 +1,20 @@
 package br.com.scm.orderservice.external.client;
 
+import br.com.scm.orderservice.exception.CustomException;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+@CircuitBreaker(name="external", fallbackMethod = "fallback")
 @FeignClient(name = "PRODUCT-SERVICE/products")
 public interface ProductService {
     @PutMapping("/reduceQuantity/{id}")
     ResponseEntity<Void> reduceQuantity(@PathVariable("id") long productId, @RequestParam long quantity);
+
+    default void fallback(Exception e) {
+        throw new CustomException("Product Service is not available", "UNAVAILABLE", 500);
+    }
 }
