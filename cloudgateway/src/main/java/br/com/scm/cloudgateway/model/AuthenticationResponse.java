6888ac6990b8ca36d31822871ba09a991bcf851e
marcoamolinaro <molinaromarcoaurelio@gmail.com>
2023-05-09 14:53:15 -0300

    Add Authenticate Login API

---
+++ cloudgateway/src/main/java/br/com/scm/cloudgateway/model/AuthenticationResponse.java
@@ -0,0 +1,21 @@
+package br.com.scm.cloudgateway.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Collection;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class AuthenticationResponse {
+
+    private String userId;
+    private String accessToken;
+    private String refreshToken;
+    private long expireAt;
+    private Collection<String> authorityList;
+}
